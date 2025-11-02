727830d30e60744910981d13c06635616cc21888
rurig <rurig@users.noreply.github.com>
2025-11-02 17:58:37 +0200

    incercare noua

--- clase.h
+++ clase.h
@@ -6,54 +6,40 @@
 #include <ostream>
 using namespace std;
 
-//                                        :3
-
 class Cat {
 private:
     string name;
-    int evilness;
-    int cuteness;
-    int hunger;
-    int loyalty;
-
+    int evilness, cuteness, hunger, loyalty;
 public:
     Cat(string n, int e, int c, int h, int l);
-    Cat(const Cat &other);
+    Cat(const Cat&);
+    Cat& operator=(const Cat&);
     ~Cat();
-    Cat& operator=(const Cat& other);
-    friend ostream& operator<<(ostream& os, const Cat& c);
+
+    friend ostream& operator<<(ostream&, const Cat&);
 
     [[nodiscard]]const string& getName() const;
-    [[nodiscard]]int getEvilness() const;
+   [[nodiscard]] int getEvilness() const;
     [[nodiscard]]int getCuteness() const;
     [[nodiscard]]int getHunger() const;
     [[nodiscard]]int getLoyalty() const;
 
-    void feed(int cant);
-    void trainEvil(int cant);
-    void rewardLoyalty(int cant);
-    void increaseHunger(int cant);
+    void feed(int);
+    void trainEvil(int);
+    void rewardLoyalty(int);
+    void increaseHunger(int);
 };
-
-//                                           :3
 
 class Humanity {
 private:
-    int suspicion;
-    int maxSuspicion;
-
+    int suspicion, maxSuspicion;
 public:
     explicit Humanity(int start = 0, int maxS = 100);
-
-    void increaseSuspicion(int val);
-    void decreaseSuspicion(int val);
-
+    void increaseSuspicion(int);
+    void decreaseSuspicion(int);
     [[nodiscard]]bool isGameOver() const;
-
-    friend ostream& operator<<(ostream& os, const Humanity& h);
+    friend ostream& operator<<(ostream&, const Humanity&);
 };
-
-//                                         :3
 
 class Mission {
 public:
@@ -61,49 +47,48 @@ public:
 
 private:
     string name;
-    int difficulty;
-    int rewardMoney;
-    int rewardChaos;
-    int hungerCost;
-    int minEvilness;
-    int minLoyalty;
-    int minCuteness;
+    int difficulty, rewardMoney, rewardChaos;
+    int hungerCost, minEvilness, minLoyalty, minCuteness;
     MissionType type;
 
 public:
-    Mission(const string& n, int diff, int money, int chaos, int hunger, int minE, int minL, int minC, MissionType t);
+    Mission(const string&, int, int, int, int, int, int, int, MissionType);
+    static Mission randomMission();
 
-    [[nodiscard]]const string& getName() const { return name; }
-    [[nodiscard]]int getRewardMoney() const { return rewardMoney; }
-    [[nodiscard]]int getRewardChaos() const { return rewardChaos; }
-    [[nodiscard]]int getHungerCost() const { return hungerCost; }
-    [[nodiscard]]int getDifficulty() const { return difficulty; }
-    [[nodiscard]]MissionType getType() const { return type; }
+    [[nodiscard]]const string& getName() const;
+    [[nodiscard]]int getRewardMoney() const;
+    [[nodiscard]]int getRewardChaos() const;
+    [[nodiscard]]int getHungerCost() const;
+    [[nodiscard]]int getDifficulty() const;
+    [[nodiscard]]MissionType getType() const;
 
-    [[nodiscard]]bool attempt(const Cat& c) const;
-
-    friend ostream& operator<<(ostream& os, const Mission& m);
+    [[nodiscard]]bool attempt(const Cat&) const;
+    friend ostream& operator<<(ostream&, const Mission&);
 };
-
-//                                          :3
 
 class CatOverlord {
 private:
     vector<Cat> cats;
-    int money;
-    int chaosPoints;
-    int actionPoints;
+    vector<Mission> missions;
+    int money, chaosPoints, actionPoints;
 
 public:
     explicit CatOverlord(int startMoney = 50, int startChaos = 10, int startAP = 6);
-    friend ostream& operator<<(ostream& os, const CatOverlord& o);
 
-    void addCat(const Cat& c);
-    void feedCat(int index, int cant);
-    void encourageCat(int index, int cant);
+    friend ostream& operator<<(ostream&, const CatOverlord&);
+
+    void addCat(const Cat&);
+    void feedCat(int, int);
+    void encourageCat(int, int);
+    void trainCatEvil(int, int);
+    bool sendOnMission(int, const Mission&, Humanity&);
     void nextDay();
-    bool sendOnMission(int index, const Mission& m, Humanity& humans);
-    void trainCatEvil(int index, int cant);
+
+    // NEW
+    Cat generateRandomCat();
+    void adoptCat();
+    vector<Mission>& getMissions();
+
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,91 +3,48 @@
 using namespace std;
 
 int main() {
-    //pisici
-    Cat cat1("Magu", 50, 80, 30, 80);
-    Cat cat2("Dracat Mihaita", 80, 20, 60, 50);
-    Cat cat3("Lucius", 30, 90, 20, 90);
+    srand(time(nullptr));
 
-    //overlord
     CatOverlord overlord;
-    overlord.addCat(cat1);
-    overlord.addCat(cat2);
-    overlord.addCat(cat3);
-
-    //humanity
     Humanity humans;
 
-    //misiuni
-    Mission bankHeist("Steal Socks", 15, 5, 30, 25, 35, 10, 0, Mission::EVIL);
-    Mission pizzaSteal("Infiltrate the government", 45, 40, 5, 5, 66, 50, 0, Mission::EVIL);
-    Mission funnyCompilation("Funny Cat Compilation", 5, 0, 0, 5, 0, 0, 20, Mission::PR);
+    overlord.addCat(Cat("Magu", 50,80,30,80));
+    overlord.addCat(Cat("Dracat",80,20,60,50));
+    overlord.addCat(Cat("Lucius",30,90,20,90));
 
-    bool gameEnd = false;
-    while (gameEnd == false) {
-        cout << "\n Cat Overlord Status " << endl;
-        cout << overlord << endl;
-        cout << humans << endl;
+    bool end = false;
+    while (!end) {
+        cout << "\n=== CAT OVERLORD ===\n" << overlord << humans << "\n";
+        cout << "1.Feed 2.Encourage 3.Train Evil 4.Send Mission\n";
+        cout << "5.Next Day 6.Quit 7.Adopt Cat 8.Show Missions\n> ";
 
-        cout << "\nChoose action:\n";
-        cout << "1. Feed a cat\n2. Encourage a cat\n3. Train cat Evilness\n4. Send cat on mission\n5. Next day\n6. Quit\n> ";
+        int ch; cin >> ch;
+        int i, amt, m;
 
-        int choice;
-        cin >> choice;
-
-        int index, amount;
-        switch (choice) {
-            case 1:
-                cout << "Select cat index to feed: ";
-                cin >> index;
-                cout << "Amount to feed: ";
-                cin >> amount;
-                overlord.feedCat(index, amount);
-                break;
-            case 2:
-                cout << "Select cat index to encourage: ";
-                cin >> index;
-                cout << "Amount to increase loyalty: ";
-                cin >> amount;
-                overlord.encourageCat(index, amount);
-                break;
-            case 3:
-                cout << "Select cat index to train evilness: ";
-                cin >> index;
-                cout << "Amount to train evilness: ";
-                cin >> amount;
-                overlord.trainCatEvil(index, amount);
-                break;
+        switch(ch) {
+            case 1: cout<<"Cat index:";cin>>i; cout<<"Food:";cin>>amt; overlord.feedCat(i,amt); break;
+            case 2: cout<<"Cat index:";cin>>i; cout<<"Loyalty:";cin>>amt; overlord.encourageCat(i,amt); break;
+            case 3: cout<<"Cat index:";cin>>i; cout<<"Evil:";cin>>amt; overlord.trainCatEvil(i,amt); break;
             case 4:
-                cout << "Select cat index to send on mission: ";
-                cin >> index;
-                cout << "Select mission:\n1. Bank Heist\n2. Steal Pizza\n3. Funny Compilation\n> ";
-                int missionChoice;
-                cin >> missionChoice;
-                if (missionChoice == 1)
-                    gameEnd = overlord.sendOnMission(index, bankHeist, humans);
-                else if (missionChoice == 2)
-                    gameEnd = overlord.sendOnMission(index, pizzaSteal, humans);
-                else if (missionChoice == 3)
-                    gameEnd = overlord.sendOnMission(index, funnyCompilation, humans);
+                cout<<"Cat index:";cin>>i;
+                cout<<"Missions:\n";
+                for (int k=0;k<(int)overlord.getMissions().size();k++)
+                    cout<<"["<<k<<"] "<<overlord.getMissions()[k]<<endl;
+                cin>>m;
+                end = overlord.sendOnMission(i, overlord.getMissions()[m], humans);
                 break;
-            case 5:
-                overlord.nextDay();
-                cout << "Day advanced. Action points reset and hunger increased.\n";
-                break;
-            case 6:
-                gameEnd = true;
+            case 5: overlord.nextDay(); break;
+            case 6: end = true; break;
+            case 7: overlord.adoptCat(); break;
+            case 8:
+                for (int k=0;k<(int)overlord.getMissions().size();k++)
+                    cout<<"["<<k<<"] "<<overlord.getMissions()[k]<<endl;
                 break;
             default:
-                cout << "Invalid choice!\n";
+                cout<<"Invalid input!\n";
+                break;
         }
-
-        if (humans.isGameOver()) {
-            cout << "Game Over! Humans discovered the cat conspiracy!\n";
-            break;
-        }
+        if (humans.isGameOver()) { cout<<"Humans discovered cats! GAME OVER\n"; break; }
     }
-
-    cout << ":3";
-
     return 0;
 }
